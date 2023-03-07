63b57ddefbaf8e2b64e6dd0cf3a241dee8b2bdbc
Gregory <elvismabisi@gmail.com>
2023-03-07 13:02:38 +0300

    new changes

--- project-manager/src/App.js
+++ project-manager/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
 
+import LandingPage from './components/LandingPage'
 import LoginPage from './components/LoginPage'
 import Registration from './components/Registration'
 import HomePage from './components/HomePage'
@@ -10,6 +11,7 @@ function App() {
   <Router>
       <div>
           <Routes>
+              <Route path="/" element={ <LandingPage/> } />
               <Route path="/login" element={ <LoginPage/> } />
               <Route path="/register" element={ <Registration />} />
               <Route path="/home" element={< HomePage/> } />
